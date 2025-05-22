5edb5c862f2a06d5e16b6f240334a6e86ee5137a
ErnestManigoJr <mrernestmanigoJr@gmail.com>
2025-05-22 07:59:33 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const uploadRoute = require('./routes/upload');
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use('/upload', uploadRoute);
 
 app.get('/', (req, res) => {
   res.send('Quincy Backend Running');
